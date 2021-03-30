0ba433da485dade1d499519ad980eadce27b8005
zhanglu97 <1067351553@qq.com>
2021-03-30 15:30:29 +0800

    文本修改

--- src/renderer/page/mainContent/Settingmanage/setting_text.vue
+++ src/renderer/page/mainContent/Settingmanage/setting_text.vue
@@ -6,6 +6,13 @@
             </div>
             <textarea @input="handleTextareaInput" v-model="areatext" onscroll="document.getElementById('leftNum').scrollTop = this.scrollTop;" id="edit-content"></textarea>
         </div>
+        
+        <div class="addForm zll-form">
+            <div id="leftBox">
+                <textarea wrap="off" cols="2" id="leftNum2" disabled></textarea>
+            </div>
+            <div class="txt" contentEditable="true" ref="div1" v-html="innerText"  @input="changeText" @keydown="keydown" onscroll="document.getElementById('leftNum2').scrollTop = this.scrollTop;"></div>
+        </div>
     </div>
 </template>
 <script>
@@ -13,7 +20,9 @@ export default {
     data() {
         return {
             num: "",
+            num2: "",
             areatext: "",
+            innerText: '',
             content: "",
             settingList: [
                 {
@@ -62,9 +71,49 @@ export default {
             lineobj.value = this.num;
             this.num = "";
         },
+        keepLastIndex(obj) {
+            if (window.getSelection) {//ie11 10 9 ff safari
+                obj.focus(); //解决ff不获取焦点无法定位问题
+                var range = window.getSelection();//创建range
+                range.selectAllChildren(obj);//range 选择obj下所有子内容
+                range.collapseToEnd();//光标移至最后
+            }
+            else if (document.selection) {//ie10 9 8 7 6 5
+                var range = document.selection.createRange();//创建选择对象
+                //var range = document.body.createTextRange();
+                range.moveToElementText(obj);//range定位到obj
+                range.collapse(false);//光标移至最后
+                range.select();
+            }
+        },
+        changeText(e) {
+            var text = e.target.innerText
+            if(text) {
+                text = text.replace(/\r/gi, "");
+                text = text.split("\n");
+                var n = text.length
+                var lineobj = document.getElementById("leftNum2");
+                for (var i = 1; i <= n; i++) {
+                    if (document.all) {
+                        this.num2 += i + "\r\n"; //判断浏览器是否是IE
+                    } else {
+                        this.num2 += i + "\n";
+                    }
+                }
+                lineobj.value = this.num2;
+                this.num2 = "";
+                
+                this.innerText = e.target.innerText;
+                this.innerText = this.innerText.replace(/(\s*=\s*)/g, `<span style="color: red">=</span>`).replace(/\n/g, '<br/>')
+            }
+        },
+        keydown(e) {
+            this.keepLastIndex(e.target)
+        }
     },
     mounted() {
         this.handleTextareaInput({ target: { value: this.areatext } });
+        this.changeText({ target: { value: this.innerText } });
     },
     components: {},
 
@@ -93,7 +142,7 @@ export default {
     text-align: left;
     border: 1px solid #eaeaea;
 }
-#edit-content {
+#edit-content, .txt {
     padding: 10px 8px;
     width: calc(100% - 40px);
     height: 100%;
@@ -106,8 +155,9 @@ export default {
     box-sizing: border-box;
     outline: none;
     resize: none;
+    overflow: scroll;
 }
-#leftNum {
+#leftNum, #leftNum2 {
     padding: 10px 4px;
     height: 100%;
     width: 100%;
